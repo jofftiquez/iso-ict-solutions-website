d4b79ac8e214e57f8a5ce27c818f213270845d32
Jofferson Ramirez Tiquez <jofftiquez@gmail.com>
2020-05-14 12:52:59 +0800

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,90 @@
+import colors from 'vuetify/es5/util/colors'
+
+export default {
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s',
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv'
+  ],
+  /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    theme: {
+      themes: {
+        // NOTE: These colors are MYCURE defaults
+        light: {
+          primary: '#2e9fdf',
+          accent: '#7fad33',
+          secondary: '#616161',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: '#ff5252',
+          success: colors.green.accent3,
+        },
+        dark: {
+          primary: '#2e9fdf',
+          accent: '#7fad33',
+          secondary: '#616161',
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: '#ff5252',
+          success: colors.green.accent3,
+        },
+      }
+    }
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+  v-container
+    //- TODO: Remove this
+    sample
+</template>
+
+<script>
+import Sample from '~/components/sample';
+import headMeta from '~/utils/head-meta';
+export default {
+  components: {
+    Sample,
+  },
+  head () {
+    return headMeta({
+      title: 'Sample Web Page',
+      description: 'A simple web page in this fresh setup.',
+    });
+  },
+};
+</script>
