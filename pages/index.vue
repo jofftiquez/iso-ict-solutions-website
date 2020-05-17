d3d16e153242001087adbbfe073646325a0389cf
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-17 19:43:43 +0800

    refactor: add new contents

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,6 @@
 <template lang="pug">
   v-content
-    v-toolbar(
-      app
-      flat
-      absolute
-      color="transparent"
-      width="100%"
-    )
-      h1.white--text ISO: ICT Solutions
-      v-spacer
-      v-btn(depressed large text color="white").ma-1 The Company
-      v-btn(depressed large text color="white").ma-1 Services
-      v-btn(depressed large text color="white").ma-1 Contact Us
+    iso-toolbar
     v-container(fluid style="height: 100vh").fill-height.panel-1
       v-row(justify="center" align="center")
         v-col(cols="12")
@@ -23,44 +12,149 @@
             v-card-text.text-center
               v-btn(icon large).primary
                 v-icon(style="font-size: 50px").white--text mdi-chevron-down
-    v-container(style="height: 100vh").fill-height
-      v-row(justify="center" align="center")
-        v-col(cols="12" md="4").text-center.pa-10
-          img(width="100%" src="../assets/images/the-company.svg")
-        v-col(cols="12" md="8").pa-10
+    v-container(id="the-company")
+      v-row(justify="center" align="center").pa-10
+        v-col(cols="12" md="6").text-center.pa-10
+          img(:width="$isMobile ? '100%' : '350'" src="../assets/images/the-company.svg")
+        v-col(cols="12" md="6").pa-10
           h1.primary--text The Company
-          p Incididunt dolore commodo aliqua aliquip dolor dolor ullamco mollit reprehenderit proident commodo et sunt sunt. Quis do ea labore consequat ut irure qui. Veniam ullamco ut sint deserunt do excepteur. Sint veniam mollit excepteur aliqua aute sint ex quis. Cillum qui nulla reprehenderit elit aliqua dolore officia dolore fugiat laboris nulla.
-          p Eiusmod aute elit magna est. Nisi nostrud anim in velit. Sint cupidatat anim dolor id anim in et ut occaecat ea consequat. Non ipsum dolor adipisicing exercitation nostrud enim. Ullamco consectetur quis voluptate sunt mollit incididunt nulla sint.
-    v-container(style="height: 100vh").fill-height
-      v-row(justify="center" align="center")
-        v-col(cols="12" md="8").pa-10
+          p Innovative Smart Options: ICT Solutions acts as your back office and reliable business partner available 24/7 via online means. Our highly trained, certified and well-experienced professionals will work and prepare any phase or phases of your project scope. ISO: ICT Solutions helps you save time and cost using an entirely new approach.
+          p Challenging times demand innovative strategies in all aspects of activities from project pre-sales preparation, requirement review, development, deployment, monitoring, and completion to post-project services delivery.
+          p We understand your financial and social constraints in organizing your team and we further support your commitment to a successful project hand-over.
+    v-container(id="the-team")
+      v-row(justify="center" align="center").pa-10
+        v-col(cols="12" md="6").pa-10
+          h1.primary--text The Team
+          p A group of highly experienced and technically knowledgeable professionals conceptualized the idea of supporting ICT companies who had been affected by this “new normal” business environment.
+          p Innovative Smart Options: ICT Solutions offers help to those organizations who had downsized their teams, those on start up or even existing companies who opted to outsource the services of ICT consultants.
+        v-col(cols="12" md="6").text-center.pa-10
+          img(:width="$isMobile ? '100%' : '300'" src="../assets/images/the-team.svg")
+    v-container(id="services")
+      v-row(justify="center" align="center").pa-10
+        v-col(cols="12").text-center.pa-10
+          img(width="200" src="../assets/images/our-services.svg")
           h1.primary--text Range of Support Services
-          p Incididunt dolore commodo aliqua aliquip dolor dolor ullamco mollit reprehenderit proident commodo et sunt sunt. Quis do ea labore consequat ut irure qui. Veniam ullamco ut sint deserunt do excepteur. Sint veniam mollit excepteur aliqua aute sint ex quis. Cillum qui nulla reprehenderit elit aliqua dolore officia dolore fugiat laboris nulla.
-          ul
-            li Consectetur labore velit occaecat aliqua proident mollit exercitation.
-            li Culpa pariatur mollit est magna velit.
-            li Magna id adipisicing consectetur fugiat culpa eiusmod.
-        v-col(cols="12" md="4").text-center.pa-10
-          img(width="300" src="../assets/images/our-services.svg")
-    v-footer(height="auto" color="primary" dark)
+        v-col(cols="12" md="10")
+          v-card
+            v-card-text
+              h3.primary--text Consultancy Services: BICSI certified Professionals
+              ul
+                li RCDD (Registered Communications Distribution Designer)
+                li DCDC (Data Center Design Consultant)
+                li OSP (Outside Plant Designer)
+        v-col(cols="12" md="10")
+          v-card
+            v-card-text
+              h3.primary--text Pre-sales Preparation
+              ul
+                li BOQ (Bill of Quantity)
+                  ul
+                    li Review of project specifications
+                    li RFI (Request for Information)
+                    li Costing Sheets
+                li Design Preparation
+                  ul
+                    li Schematic Drawings
+                    li Detailed Design
+                    li Issued-for-Construction Drawings
+                    li Shop Drawings
+                    li Redline Mark-ups
+                    li As-built Drawings
+                    li Resubmissions of layouts
+                li Documentation
+                  ul
+                    li Weekly Accomplishment Reports/Project Planning
+                    li Technical Submittals
+                    li Method Statements
+                    li Project Execution RFIs
+                    li Labeling Schemes
+                    li Mock-up
+                    li Operation and Maintenance Manual
+                li Construction Phase
+                  ul
+                    li Quality Control
+                    li Site validation
+                    li Team deployment for:
+                      ol
+                        li Installation
+                        li Testing
+                li Acceptance Phase
+                  ul
+                    li Documentation Submittals
+        v-col(cols="12" md="10")
+          v-card
+            v-card-text
+              h3.primary--text Range of Systems
+              ul
+                li Structured Cabling Systems, facilities for
+                  ul
+                    li Residential
+                    li Business
+                    li Mixed-Use
+                    li Hospital
+                    li Maritime
+                    li Military
+                    li Airport
+                    li Tunnel
+                    li Railroad
+                    li Warehouses
+                    li Parking Areas
+                    li Others
+                li Data Center Cabling and Containment, OSP Projects, Security Systems
+                  ul
+                    li CCTV and various applications
+                    li Access Control
+                    li Public Address
+                    li Audio-Visual
+                li Fire Detection and Alarm
+    v-footer(height="auto"  dark)
       v-container
-        v-row
-          v-col
-            h1 foo
+        v-row(justify="center").pa-10
+          v-col(xs="12" md="3")
+            b.primary--text Sitemap
+            ul.no-decoration-ul
+              li
+                a(href="#the-company") The Company
+              li
+                a(href="#the-team") The Team
+              li
+                a(href="#services") Services
+          v-col(xs="12" md="3")
+            b.primary--text Follow Us
+            ul.no-decoration-ul
+              li
+                a Facebook
+              li
+                a Twitter
+          v-col(xs="12" md="3")
+            b.primary--text Contact Us
+            ul.no-decoration-ul
+              li email@sample.com
+              li +639165008232
+              li
+                a(@click="toggleChat") Live Chat
+        v-row(justify="center")
+          v-col(cols="12").text-center
+            p Copyright © {{new Date().getFullYear()}} #[span.font-weight-black.primary--text Innovative Smart Options: ICT Solutions]. All Rights Reserved.
 </template>
 
 <script>
-import Sample from '~/components/sample';
+import IsoToolbar from '~/components/Toolbar';
 import headMeta from '~/utils/head-meta';
 export default {
   components: {
-    Sample,
+    IsoToolbar,
   },
   head () {
     return headMeta({
-      title: 'Sample Web Page',
-      description: 'A simple web page in this fresh setup.',
+      title: 'Innovative Smart Options: ICT Solutions',
+      description: 'Innovative Smart Options: ICT Solutions',
     });
+  },
+  methods: {
+    toggleChat () {
+      window.$crisp.push(['do', 'chat:toggle']);
+    },
   },
 };
 </script>
@@ -70,5 +164,18 @@ export default {
   background-image: url('../assets/images/usp.jpg');
   background-position: center center;
   background-size: cover;
+}
+
+.no-decoration-ul {
+  list-style: none;
+  padding-left: 0;
+}
+
+a {
+  color: white;
+}
+
+a:hover {
+  color: #009688;
 }
 </style>
