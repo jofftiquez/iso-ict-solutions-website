ac300b6a84fabe387b066a9f26b08d4d9ad66660
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-24 18:32:19 +0800

    feat: create component for company profile

---
+++ components/home/TheCompany.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+  v-container(id="the-company")
+    v-row(justify="center" align="center").pa-10
+      //- v-col(cols="12" md="6").text-center
+      //-   img(width="100%" src="../../assets/images/the-company.jpg")
+      v-col(cols="12" md="12").text-center
+        h1(:class="{ 'display-3': !$isMobile, 'display-2': $isMobile }").display-4.font-weight-black The Company
+        h2.primary--text Add some subheading here for more content
+        br
+        p.text-justify Innovative Smart Options: ICT Solutions acts as your back office and reliable business partner available 24/7 via online means. Our highly trained, certified and well-experienced professionals will work and prepare any phase or phases of your project scope. ISO: ICT Solutions helps you save time and cost using an entirely new approach.
+        p.text-justify Challenging times demand innovative strategies in all aspects of activities from project pre-sales preparation, requirement review, development, deployment, monitoring, and completion to post-project services delivery.
+        p.text-justify We understand your financial and social constraints in organizing your team and we further support your commitment to a successful project hand-over.
+      //- v-col(cols="12" md="6").text-center.pa-10
+      //-   img(:width="$isMobile ? '100%' : '350'" src="~/assets/images/the-company.svg")
+      //- v-col(cols="12" md="6").pa-10
+      //-   h1.primary--text The Company
+      //-   p Innovative Smart Options: ICT Solutions acts as your back office and reliable business partner available 24/7 via online means. Our highly trained, certified and well-experienced professionals will work and prepare any phase or phases of your project scope. ISO: ICT Solutions helps you save time and cost using an entirely new approach.
+      //-   p Challenging times demand innovative strategies in all aspects of activities from project pre-sales preparation, requirement review, development, deployment, monitoring, and completion to post-project services delivery.
+      //-   p We understand your financial and social constraints in organizing your team and we further support your commitment to a successful project hand-over.
+</template>
