c5366d3fc5f48ecb24c0885c1bc566f78e00e1bd
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-25 11:04:34 +0800

    refactor: wrap carousel in a div

--- components/home/Usp.vue
+++ components/home/Usp.vue
@@ -1,50 +1,40 @@
 <template lang="pug">
-  v-carousel(
-    cycle
-    hide-delimiter-background
-    show-arrows-on-hover
-    height="100vh"
-    reverse-transition="fade-transition"
-    transition="fade-transition"
-  )
-    v-carousel-item(
-      v-for="(item, key) in carouselItems"
-      :src="item.src"
-      :key="key"
+  div#top
+    v-carousel(
+      cycle
+      hide-delimiter-background
+      show-arrows-on-hover
+      height="100vh"
+      reverse-transition="fade-transition"
+      transition="fade-transition"
     )
-      v-sheet(
-        height="100%"
-        color="transparent"
-        dark
+      v-carousel-item(
+        v-for="(item, key) in carouselItems"
+        :src="item.src"
+        :key="key"
       )
-        v-row(
-          class="fill-height"
-          align="center"
-          justify="center"
+        v-sheet(
+          height="100%"
+          color="transparent"
+          dark
         )
-          v-col(cols="12")
-            v-card(flat color="transparent")
-              v-card-text.text-center
-                h1.white--text.display-3.font-weight-black Innovative Smart Options:
+          v-row(
+            class="fill-height"
+            align="center"
+            justify="center"
+          )
+            v-col(cols="12")
+              v-card(flat color="transparent")
+                v-card-text.text-center
+                  h1.white--text.display-3.font-weight-black Innovative Smart Options:
+                    br
+                    | ICT Solutions
                   br
-                  | ICT Solutions
-                br
-                br
-                h2.white--text.display-2 Works for you!
-              v-card-text.text-center
-                v-btn(href="#the-company" icon large).primary
-                  v-icon(style="font-size: 50px").white--text mdi-chevron-down
-  //- v-container(fluid style="height: 100vh").fill-height.panel-1
-    v-row(justify="center" align="center")
-      v-col(cols="12")
-        v-card(flat color="transparent")
-          v-card-text.text-center
-            h1.black--text.display-3.font-weight-black Innovative Smart Options: ICT Solutions
-            br
-            h2.black--text.display-2 Works for you! {{$isMobile}}
-          v-card-text.text-center
-            v-btn(icon large).primary
-              v-icon(style="font-size: 50px").white--text mdi-chevron-down
+                  br
+                  h2.white--text.display-2 Works for you!
+                v-card-text.text-center
+                  v-btn(href="#" icon large v-scroll-to="{ el: '#the-company', offset: scrollOffset }").primary
+                    v-icon(style="font-size: 50px").white--text mdi-chevron-down
 </template>
 
 <script>
@@ -63,6 +53,11 @@ export default {
         },
       ],
     };
+  },
+  computed: {
+    scrollOffset () {
+      return this.$isMobile ? -56 : -64;
+    },
   },
 };
 </script>
