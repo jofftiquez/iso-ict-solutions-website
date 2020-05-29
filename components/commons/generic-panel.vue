1016d7191f356553a26f50466e8e92cfb5c32de7
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-29 10:07:01 +0800

    feat: create generic-panel component

---
+++ components/commons/generic-panel.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div(v-bind="wrapperBindings")
+    v-container(:class="{ 'fill-height': fullHeight }" :style="{ height: fullHeight ? '100vh' : '' }")
+      v-row(v-bind="headRowBindings")
+        slot(name="head")
+      v-row(v-bind="contentRowBindings")
+        slot(name="content")
+      v-row(v-bind="footerRowBindings")
+        slot(name="footer")
+</template>
+
+<script>
+export default {
+  props: {
+    wrapperBindings: {
+      type: Object,
+      default: () => ({}),
+    },
+    headRowBindings: {
+      type: Object,
+      default: () => ({}),
+    },
+    contentRowBindings: {
+      type: Object,
+      default: () => ({}),
+    },
+    footerRowBindings: {
+      type: Object,
+      default: () => ({}),
+    },
+    fullHeight: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
