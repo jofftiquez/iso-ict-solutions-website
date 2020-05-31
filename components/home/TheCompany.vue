f599b1e32a44d9246cf0bfc167b1b3c0171fcf3e
Jofferson R Tiquez <jofftiquez@gmail.com>
2020-05-31 16:45:24 +0800

    refactor: update content

--- components/home/TheCompany.vue
+++ components/home/TheCompany.vue
@@ -4,8 +4,8 @@
       //- v-col(cols="12" md="6").text-center
       //-   img(width="100%" src="../../assets/images/the-company.jpg")
       v-col(cols="12" md="12").text-center
-        h1(:class="{ 'display-3': !$isMobile, 'display-2': $isMobile }").display-4.font-weight-black The Company
-        h2.primary--text Add some subheading here for more content
+        h1(:class="{ 'display-3': !$isMobile, 'display-2': $isMobile }").display-4.font-weight-black We Innovative
+        h2.primary--text About Us
         br
         p.text-justify Innovative Smart Options: ICT Solutions acts as your back office and reliable business partner available 24/7 via online means. Our highly trained, certified and well-experienced professionals will work and prepare any phase or phases of your project scope. ISO: ICT Solutions helps you save time and cost using an entirely new approach.
         p.text-justify Challenging times demand innovative strategies in all aspects of activities from project pre-sales preparation, requirement review, development, deployment, monitoring, and completion to post-project services delivery.
